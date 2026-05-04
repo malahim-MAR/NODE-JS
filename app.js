09a9446b6e9f9e6199b668240294cb04e62350d7
malahim-mar <armalahim01@gmail.com>
2026-05-04 11:45:01 +0500

    Setup Express

---
+++ app.js
@@ -0,0 +1,23 @@
+// Testing Express Setup
+// const express = require("express");
+// const app = express();
+
+// // this tells express to automatically parse JSON bodies
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my app");
+// });
+
+// app.post("/users", (req, res) => {
+//   const user = req.body;
+//   console.log("Received:", user);
+//   res.status(201).json({ message: "User created", user });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
+
+
+// Creating First EVER API 
