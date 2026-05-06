bf4eba6ebaff48beb6d2644f6ade67d211e43c8d
malahim-mar <armalahim01@gmail.com>
2026-05-06 07:12:44 +0500

    First Custom API BUILD

--- app.js
+++ app.js
@@ -19,5 +19,34 @@
 //   console.log("Server running on http://localhost:3000");
 // });
 
+// Creating First EVER API
 
-// Creating First EVER API 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "Malahim", age: 19, role: "Web development" },
+  { id: 2, name: "Abdul Rehman", age: 22, role: "Mobile App development" },
+];
+app.get("/", (req, res) => {
+  res.send("Welcome to my app");
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  if (!user) {
+    return res.status(404).json({ message: "user not found" });
+  }
+  res.status(200).json(user);
+});
+
+app.listen(3000, () => {
+  console.log("server is running on http://localhost:3000");
+});
